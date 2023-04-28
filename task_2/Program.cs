86647f59794e72faafc1a602b3b5435847875fe8
Tatyana <tatyana@gus.ru>
2023-04-28 16:59:19 +0300

    Вторая задача

---
+++ task_2/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] Generate2DArray(int cntRows, int cntColumns, int minLimit, int maxLimit)
+{
+    int[,] array = new int[cntRows, cntColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minLimit, maxLimit + 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[] SumRow(int[,] array)
+{
+    int[] result = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+        result[i] = sum;
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+
+    }
+}
+
+
+int rows = ReadInt("Введите количество строк");
+int columns = ReadInt("Введите количество столбцов");
+int minLimit = ReadInt("Минимальное случайное значение");
+int maxLimit = ReadInt("Максимальное случайное значение");
+int[,] arr = Generate2DArray(rows, columns, minLimit, maxLimit);
+Print2DArray(arr);
+System.Console.WriteLine();
+// SumRow(arr);
+PrintArray(SumRow(arr));
